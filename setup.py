e65234d2de822121e105271b212a7d356b965c17
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-09-14 15:33:15 -0700

    correct license name

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     url='https://github.com/ch4zm/blaseball-game-finder',
     author='Ch4zm of Hellmouth',
     author_email='ch4zm.of.hellmouth@gmail.com',
-    license='BSD-3-Clause',
+    license='MIT',
     entry_points="""
     [console_scripts]
     {program} = game_finder.command:main
